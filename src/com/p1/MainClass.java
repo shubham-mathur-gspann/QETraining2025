2e36dd8e00502d9458eca4c15abf946c2b3a0afa
Priyanshu Pandey <priyanshu.pandey@gspann.com>
2025-07-14 16:18:41 +0530

    final

--- src/com/p1/MainClass.java
+++ src/com/p1/MainClass.java
@@ -3,14 +3,8 @@ package com.p1;
 public class MainClass {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-
 		
 		System.out.println("Priyanshu Pandey");
-
-=======
-		System.out.println("<Pranshu");
->>>>>>> feature/Pranshu
 
 	}
 
