7674f6cbfe0681495cb0c1e446274d4a855435d5
Rashika Sharma <rashika.sharma@gspann.com>
2025-07-14 16:42:13 +0530

    New

--- src/com/p1/MainClass.java
+++ src/com/p1/MainClass.java
@@ -8,12 +8,8 @@ public class MainClass {
 
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Rashika Sharma");
-=======
 
-		System.out.println("MayankGupta");
->>>>>>> edd7a9758035c3b7bb6034edb03c9a3e678b627b
+		System.out.println("Rashika Sharma");
 
 	}
 
