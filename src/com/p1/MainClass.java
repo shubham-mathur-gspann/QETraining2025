8020d8af97b975561bf92ed056174572d021b077
Mohammed Hasan <mohammed.hasan@gspann.com>
2025-07-14 20:15:48 +0530

    Mohammed hasan final commit 20:15

--- src/com/p1/MainClass.java
+++ src/com/p1/MainClass.java
@@ -3,7 +3,7 @@ package com.p1;
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("Bade Anil Sai Kumar");
+		System.out.println("Mohammed Owais hasan");
 	
 }
 
