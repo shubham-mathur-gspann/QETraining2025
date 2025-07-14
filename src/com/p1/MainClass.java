6243210fff3f39469e4481a2a70f02b24ebf83ee
Priyanshu Pandey <priyanshu.pandey@gspann.com>
2025-07-14 16:20:37 +0530

    last commit

--- src/com/p1/MainClass.java
+++ src/com/p1/MainClass.java
@@ -3,16 +3,9 @@ package com.p1;
 public class MainClass {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		
 		System.out.println("Priyanshu Pandey");
-
-=======
-
-		System.out.println("Vikal Rathore");
-
-
->>>>>>> 522230286b6c56a3a2efc3341fdcea977bfb6e45
 	}
 
 }
