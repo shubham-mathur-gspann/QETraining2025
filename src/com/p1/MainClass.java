9255cef0078a23c1fa26167807e1f8120aa6e085
Vikalp.rathore <vikalp.rathore@gspann>
2025-07-14 16:16:04 +0530

    added my name

--- src/com/p1/MainClass.java
+++ src/com/p1/MainClass.java
@@ -3,16 +3,8 @@ package com.p1;
 public class MainClass {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+	System.out.println("Vikal Rathore");
 
-
-		
-		System.out.println("Abhinav Singh");
-
-
-=======
-		System.out.println("<Pranshu");
->>>>>>> feature/Pranshu
 
 	}
 
