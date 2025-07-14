4871de4b5f9f2773ca3927c6b99812bb6cf58965
Likithkumar Gajam <likithkumar.gajam@gspann.com>
2025-07-14 16:32:13 +0530

    Update the name to likith

--- src/com/p1/MainClass.java
+++ src/com/p1/MainClass.java
@@ -3,11 +3,8 @@ package com.p1;
 public class MainClass {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Dhruv Aggarwal");
-=======
-		System.out.println("<Rohan Singh was here>");
->>>>>>> ed270beba8b2417da86807410b92c7c511ac4cbb
+
+	System.out.println("Gajam Likith Kumar");
 
 	}
 
