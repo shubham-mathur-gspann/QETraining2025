ed270beba8b2417da86807410b92c7c511ac4cbb
Rohan-gspann <rohan.singh@gspann.com>
2025-07-14 15:46:05 +0530

    Rohan made the changes

--- src/com/p1/MainClass.java
+++ src/com/p1/MainClass.java
@@ -3,7 +3,7 @@ package com.p1;
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("<Fill it with your name>");
+		System.out.println("<Rohan Singh was here>");
 
 	}
 
