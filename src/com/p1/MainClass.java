a296c1cad514cef0f10097c8414011d3aeb88b5c
Priyanshu Pandey <priyanshu.pandey@gspann.com>
2025-07-15 16:30:18 +0530

    Final commit

--- src/com/p1/MainClass.java
+++ src/com/p1/MainClass.java
@@ -4,7 +4,6 @@ public class MainClass {
 
 	public static void main(String[] args) {
 
-<<<<<<< HEAD
 		System.out.println("Priyanshu Pandey");
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.google.com/");
@@ -14,9 +13,6 @@ public class MainClass {
 		String url = driver.getCurrentUrl();
 		System.out.println("URL - "+url);
 		driver.quit();
-=======
-		System.out.println("Shubham Mathur has changed this");
->>>>>>> a67c4e2b4c9dec27c0649b2c5cfc50b192e73555
 
 	} 
 		
