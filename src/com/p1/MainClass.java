b3551ba65fa8c1d420e2f7e8396efdfbe893ac4f
Aastha Kaushik <aastha.kaushik@gspann.com>
2025-07-15 16:28:22 +0530

    updated code

--- src/com/p1/MainClass.java
+++ src/com/p1/MainClass.java
@@ -1,11 +1,23 @@
 package com.p1;
+import org.openqa.selenium.WebDriver;
+ 
+import org.openqa.selenium.chrome.ChromeDriver;
+// import org.openqa.selenium.edge.EdgeDriver;
+// import org.openqa.selenium.firefox.FirefoxDriver;
+ 
 
 public class MainClass {
 
 	public static void main(String[] args) {
-
-
-		System.out.println("Aastha Kaushik");
+        WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		Thread.sleep(3000);
+		String s = driver.getTitle();
+		System.out.println("title = "+s);
+		String url =driver.getCurrentUrl();
+		System.out.println("URL = " + url);
+		driver.quit();
 
 
 	}
