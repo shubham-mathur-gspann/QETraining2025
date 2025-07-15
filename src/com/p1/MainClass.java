9b24f804f98e503cd78bdaeeb87edf1b1c3d43b9
Rashika Sharma <rashika.sharma@gspann.com>
2025-07-15 16:23:06 +0530

    Selenium Code

--- src/com/p1/MainClass.java
+++ src/com/p1/MainClass.java
@@ -1,14 +1,22 @@
-package com.p1;
+package Myseleniumcode;
 
-public class MainClass {
-
-	public static void main(String[] args) {
-
-
-		System.out.println("Rashika");
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 
-		
-	}
+public class Mycode {
 
+
+public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver(); 
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com/"); 
+        Thread.sleep(3000);
+        String title = driver.getTitle();
+        System.out.println("title=" + title);
+        String url = driver.getCurrentUrl();
+        System.out.println("current url=" + url);
+        driver.quit();
+    }
 }
