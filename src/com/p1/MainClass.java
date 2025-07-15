2cd372221d0195d9ee4825acae9aa59189e0be8d
Abhinav Singh <abhinav.singh@gspann.com>
2025-07-15 16:21:13 +0530

    Commiting after adding the selenium code

--- src/com/p1/MainClass.java
+++ src/com/p1/MainClass.java
@@ -1,10 +1,19 @@
-package com.p1;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class MainClass {
+public class OpenGoogle {
 
 	public static void main(String[] args) {
-
-		System.out.println("Abhinav Singh");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.wwe.com/");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		driver.quit();
 
 	}
 
