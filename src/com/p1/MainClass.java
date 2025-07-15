352cd482be9c1c5adb033415ad3477d962234091
Mohammed Hasan <mohammed.hasan@gspann.com>
2025-07-15 17:22:05 +0530

    Updated selenium code on remote feature

--- src/com/p1/MainClass.java
+++ src/com/p1/MainClass.java
@@ -1,11 +1,21 @@
-package com.p1;
+package com.pr;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-public class MainClass {
+public class Test {
 
-	public static void main(String[] args) {
-
-		System.out.println("Mohammed Owais Hasan");
-
-	}
+	  public static void main(String args[]) throws InterruptedException
+	  {
+		  WebDriver driver=new ChromeDriver();
+		  driver.manage().window().maximize();
+		  driver.get("https://www.youtube.com/?gl=IN");
+		  Thread.sleep(5000);
+		  
+		  System.out.println(driver.getTitle());
+		  System.out.println(driver.getCurrentUrl());
+		  driver.quit();
+		  
+		  
+	  }
 
 }
