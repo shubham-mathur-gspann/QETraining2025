411a9e940fad8ead46840232c89efee3ac2928b2
Likithkumar Gajam <likithkumar.gajam@gspann.com>
2025-07-15 19:34:31 +0530

    Likith selenium webDriver code

--- src/com/p1/MainClass.java
+++ src/com/p1/MainClass.java
@@ -1,14 +1,24 @@
 package com.p1;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MainClass {
 
-	public static void main(String[] args) {
-
-
-		System.out.println("Likith Kumar");
-
-
+	public static void main(String[] args) throws InterruptedException
+	{
+		//WebDriver driver=new EdgeDriver();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		String  title=driver.getTitle();
+		System.out.println("title="+title);
+		String url=driver.getCurrentUrl();
+		System.out.println("Url="+url);
+		driver.quit();
 		
 	}
-
+	
 }
+
+
