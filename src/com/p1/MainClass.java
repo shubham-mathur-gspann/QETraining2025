1c55ced34c53600fa128961663d4e6e219bcafac
Prince Raj <prince.raj@gspann.com>
2025-07-15 16:21:32 +0530

    Update the previous code with WebDriver code at 16:21

--- src/com/p1/MainClass.java
+++ src/com/p1/MainClass.java
@@ -1,11 +1,22 @@
 package com.p1;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-public class MainClass {
+import java.sql.SQLOutput;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
-	public static void main(String[] args) {
-
-		System.out.println("Prince Raj");
-
-	}
-
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com/");
+        Thread.sleep(3000);
+        String title = driver.getTitle();
+        System.out.println("title= "+title);
+        String url = driver.getCurrentUrl();
+        System.out.println("URL= "+url);
+        driver.quit();
+    }
 }
