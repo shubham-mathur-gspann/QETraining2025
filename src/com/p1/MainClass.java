2b3589f9585556ae7dba1375af50c17ffea5f3b1
badal-gspann <badal.singh@gspann.com>
2025-07-15 16:21:07 +0530

    updated with new code

--- src/com/p1/MainClass.java
+++ src/com/p1/MainClass.java
@@ -1,9 +1,23 @@
-package com.p1;
+package com.sel;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class MainClass {
-
-	public static void main(String[] args) {
-		System.out.println("Badal Singh");
+public class Test {
+	
+	public static void main(String... a1) throws InterruptedException
+	{
+		//WebDriver wd=new ChromeDriver();
+		WebDriver wd=new FirefoxDriver();
+		wd.manage().window().maximize();
+		wd.get("https://www.google.com/");
+		Thread.sleep(3000);
+		String title=wd.getTitle();
+		System.out.println(title+=" title");
+		String url=wd.getCurrentUrl();
+		System.out.println(url+=" Url");
+		wd.quit();
 	}
 
 }
+
