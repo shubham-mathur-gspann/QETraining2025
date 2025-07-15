474b7114afc7b8cbd2e3e57cbcc56db4b0615533
Karan Pathak <karan.pathak@gspann.com>
2025-07-15 16:18:33 +0530

    added the selenium code

--- src/com/p1/MainClass.java
+++ src/com/p1/MainClass.java
@@ -1,11 +1,24 @@
-package com.p1;
+package ggdbbgrgbr;
 
-public class MainClass {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-	public static void main(String[] args) {
-
-		System.out.println("Karan Pathak");
-
+public class Demo {
+	
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver =new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.google.com/");
+		Thread.sleep(3000);
+		String title=driver.getTitle();
+		System.out.println("title is: "+title);
+		String url=driver.getCurrentUrl();
+		System.out.println("url is: "+url);
+		driver.quit()
+;
 	}
 
 }
