8564361832c155ad55af31b5bdd0ab8552403292
anilbade-gspann <anil.bade@gspann.com>
2025-07-15 18:23:44 +0530

    commited selinium code by anil

--- src/com/p1/MainClass.java
+++ src/com/p1/MainClass.java
@@ -1,11 +1,19 @@
 package com.p1;
 
-public class MainClass {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
-	public static void main(String[] args) {
-		System.out.println("Bade Anil Sai Kumar");
+public class SecPro {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver d=new EdgeDriver();
+		d.manage().window().maximize();
+		d.get("https://www.google.com/");
+		String title = d.getTitle();
+		System.out.println("Title"+title);
+		String url = d.getCurrentUrl();
+		System.out.println("url"+url);
+		Thread.sleep(3000);
+		d.close();
 	}
-
-		
-
 }
+
