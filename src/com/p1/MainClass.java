cedb88c426564a41c58ddcf368f21d89c402fccd
Mayank Gupta <mayank.gupta@gspann.com>
2025-07-15 16:19:59 +0530

    Selenium Code

--- src/com/p1/MainClass.java
+++ src/com/p1/MainClass.java
@@ -1,11 +1,23 @@
-package com.p1;
+package Day1;
 
-public class MainClass {
+import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-	public static void main(String[] args) {
-		System.out.println("Mayank");
+public class Day1 {
+
+	public static void main(String[] args)throws InterruptedException {
+		WebDriver driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		Thread.sleep(3000);
+		String title=driver.getTitle();
+		System.out.println("title"+ title);
+		String url=driver.getCurrentUrl();
+		System.out.println("current url"+ url);
+		driver.quit();
+
+
 	}
-
-		
 
 }
