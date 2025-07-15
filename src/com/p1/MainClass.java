912da167fac1b7d88f0563bc1cf311060e187cfb
Pranshu Bhatnagar <pranshu.bhatnagar@gspann.com>
2025-07-15 16:32:06 +0530

    selenium code updated

--- src/com/p1/MainClass.java
+++ src/com/p1/MainClass.java
@@ -1,9 +1,24 @@
-package com.p1;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class MainClass {
+public class LearningSelenium {
 
-	public static void main(String[] args) {
-		System.out.println("Pranshu Bhatnagar");
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.youtube.com/");
+		Thread.sleep(3000);
+		String title = driver.getTitle();
+		String url = driver.getCurrentUrl();
+		System.out.println(title);
+		System.out.println(url);
+		driver.quit();
+
 	}
 
 }
+
+
