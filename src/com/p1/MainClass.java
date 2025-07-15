7e5ab831dfdda1c398909b693b94063f39f5f7ac
Mohammed Hasan <mohammed.hasan@gspann.com>
2025-07-15 17:25:57 +0530

    Revert "Updated selenium code on remote feature"
    This reverts commit 352cd482be9c1c5adb033415ad3477d962234091.

--- src/com/p1/MainClass.java
+++ src/com/p1/MainClass.java
@@ -1,21 +1,11 @@
-package com.pr;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+package com.p1;
 
-public class Test {
+public class MainClass {
 
-	  public static void main(String args[]) throws InterruptedException
-	  {
-		  WebDriver driver=new ChromeDriver();
-		  driver.manage().window().maximize();
-		  driver.get("https://www.youtube.com/?gl=IN");
-		  Thread.sleep(5000);
-		  
-		  System.out.println(driver.getTitle());
-		  System.out.println(driver.getCurrentUrl());
-		  driver.quit();
-		  
-		  
-	  }
+	public static void main(String[] args) {
+
+		System.out.println("Mohammed Owais Hasan");
+
+	}
 
 }
