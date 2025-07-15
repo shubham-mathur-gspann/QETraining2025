705bf26db27e87ba9b899890ef68c4e8e5cb6c77
Sakshi Aggarwal <sakshi.aggarwal@gspann.com>
2025-07-15 16:42:53 +0530

    updated selenium code

--- src/com/p1/MainClass.java
+++ src/com/p1/MainClass.java
@@ -1,9 +1,17 @@
-package com.p1;
-
-public class MainClass {
-
-	public static void main(String[] args) {
-		System.out.println("sakshi");
+package com.gspann;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.*;
+public class firstProgram {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(5000);
+	    String title = driver.getTitle();
+	    System.out.println(title);
+	     String url = driver.getCurrentUrl();
+	     System.out.println(url);
+		driver.quit();
 	}
 
 }
