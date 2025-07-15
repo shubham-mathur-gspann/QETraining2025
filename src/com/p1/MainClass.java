59a6687c6eb5c9cbcc427b96e3399b53f813eabe
Aastha Kaushik <aastha.kaushik@gspann.com>
2025-07-15 09:02:49 +0530

    Aastha's commit

--- src/com/p1/MainClass.java
+++ src/com/p1/MainClass.java
@@ -1,7 +1,13 @@
 package com.p1;
 
 public class MainClass {
+
 	public static void main(String[] args) {
-		System.out.println("Mohammed  Owais   Hasan ");
-}
+        System.out.println("<Aastha>");
+
+
+	}
+
+
+
 }
