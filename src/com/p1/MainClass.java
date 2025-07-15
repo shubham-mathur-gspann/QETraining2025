3feb005c581a40da543b8788f0dd4c66e36365a7
chanikya.konduru@gspann.com <chanikya.konduru@gspann.com>
2025-07-15 17:37:54 +0530

    Commited selenium code by chanikya

--- src/com/p1/MainClass.java
+++ src/com/p1/MainClass.java
@@ -1,10 +1,20 @@
 package com.p1;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+public class Main {
 
-public class MainClass {
-
-	public static void main(String[] args) {
-
-		System.out.println("Chanikya Konduru");
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver=new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.wikipedia.org/");
+		Thread.sleep(3000);
+		String title=driver.getTitle();
+		System.out.println("Title = "+title);
+		String url=driver.getCurrentUrl();
+		System.out.println("Current url = "+url);
+		driver.quit();
 
 	}
 
