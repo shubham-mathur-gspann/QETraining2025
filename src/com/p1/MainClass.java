fcb9a2900849b7c49146df917af6d0868d0b886b
bhuvaneswariamruthapalli-gspann <bhuvaneswari.amruthapalli@gspann.com>
2025-07-16 09:54:28 +0530

    Selenium code updated

--- src/com/p1/MainClass.java
+++ src/com/p1/MainClass.java
@@ -1,14 +1,25 @@
-package com.p1;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-public class MainClass {
+public class FirstProgram {
 
-	public static void main(String[] args) {
-
-
-		System.out.println("Bhuvaneswari");
-
-
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
 		
+		WebDriver driver=new ChromeDriver();  //launch browser
+		driver.get("https://www.google.com/");  //open URL
+		driver.manage().window().maximize();
+		
+		Thread.sleep(3000);
+		
+		String t=driver.getTitle();
+		System.out.println("Title==="+t);
+		
+		String url=driver.getCurrentUrl();
+		System.out.println("current URL"+ url);
+		
+		driver.close();
+
 	}
 
 }
